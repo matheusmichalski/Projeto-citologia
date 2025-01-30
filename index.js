e9847b1185c8ff0a79b868622a24c0a6ed87a076
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-01-30 12:19:06 -0300

    Finalização da home e footer, o header ainda está em desenvolvimento

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ acc.forEach((button) => {
     }
   });
 });
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  if (window.scrollY > 50) {
+      header.style.backgroundColor = 'white';
+  } else {
+      header.style.backgroundColor = 'transparent';
+  }
+});
+
+    const citologiaBtn = document.getElementById('citologiaBtn');
+    const citologiaPopup = document.getElementById('citologiaPopup');
+    const closeBtn = document.querySelector('.close');
+
+    citologiaBtn.onclick = function() {
+        citologiaPopup.style.display = 'block';
+    }
+
+    closeBtn.onclick = function() {
+        citologiaPopup.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target === citologiaPopup) {
+            citologiaPopup.style.display = 'none';
+        }
+    }
