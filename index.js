991a57e0f36bf5e7e5b532efd89fe69087bdec47
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-01-30 19:35:50 -0300

    Criação dos pop-up e a lista do header e ajustes na lista de referencias

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-const acc = document.querySelectorAll(".accordion");
-acc.forEach((button) => {
-  button.addEventListener("click", function () {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+//Lista Header
+
+function mostrarLista() {
+    let lista = document.getElementById("minhaLista");
+    if (lista.style.display === "none" || lista.style.display === "") {
+        lista.style.display = "block";
     } else {
-      panel.style.display = "block";
+        lista.style.display = "none";
     }
-  });
+}
+
+// Lista Referencias
+function mostrarReferencias() {
+    let lista = document.getElementById("minhasReferencias");
+    if (lista.style.display === "none" || lista.style.display === "") {
+        lista.style.display = "block";
+    } else {
+        lista.style.display = "none";
+    }
+}
+
+// Animação header
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.backgroundColor = 'white';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
 });
 
-window.addEventListener('scroll', function() {
-  var header = document.querySelector('header');
-  if (window.scrollY > 50) {
-      header.style.backgroundColor = 'white';
-  } else {
-      header.style.backgroundColor = 'transparent';
-  }
-});
+//Pop Up
+const citologiaBtn = document.getElementById('citologiaBtn');
+const citologiaPopup = document.getElementById('citologiaPopup');
+const closeBtn = document.querySelector('.close');
 
-    const citologiaBtn = document.getElementById('citologiaBtn');
-    const citologiaPopup = document.getElementById('citologiaPopup');
-    const closeBtn = document.querySelector('.close');
+citologiaBtn.onclick = function () {
+    citologiaPopup.style.display = 'block';
+}
 
-    citologiaBtn.onclick = function() {
-        citologiaPopup.style.display = 'block';
-    }
+closeBtn.onclick = function () {
+    citologiaPopup.style.display = 'none';
+}
 
-    closeBtn.onclick = function() {
+window.onclick = function (event) {
+    if (event.target === citologiaPopup) {
         citologiaPopup.style.display = 'none';
     }
+}
 
-    window.onclick = function(event) {
-        if (event.target === citologiaPopup) {
-            citologiaPopup.style.display = 'none';
-        }
-    }
